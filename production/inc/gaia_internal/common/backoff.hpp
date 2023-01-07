86a410d6eb8aa93c519f92f0e2f0de0261d9945a
Tobin Baker <tobin.d.baker@gmail.com>
2023-01-07 14:32:10 -0800

    implement backoff on contention for setting bits (no perf improvement observed)

--- production/inc/gaia_internal/common/backoff.hpp
+++ production/inc/gaia_internal/common/backoff.hpp
@@ -27,11 +27,11 @@ static void pause() { asm volatile("" ::: "memory"); }
 
 // On pre-Skylake Intel CPUs, PAUSE has latency of ~10 cycles. We assume the
 // same (for now) for AMD x86_64 CPUs.
-constexpr size_t c_pause_cycles_x86_default{10};
+static constexpr size_t c_pause_cycles_x86_default{10};
 // On Skylake, PAUSE has latency of ~140 cycles (!).
-constexpr size_t c_pause_cycles_skylake{140};
+static constexpr size_t c_pause_cycles_skylake{140};
 // On Cascade Lake and later Intel uarchs, PAUSE has latency of ~40 cycles.
-constexpr size_t c_pause_cycles_cascadelake{40};
+static constexpr size_t c_pause_cycles_cascadelake{40};
 
 // TODO: document YIELD latency for AArch64. So far it seems to have latency of
 // only 1 cycle, but I'm not sure. Also could avoid hardcoding PAUSE latencies
@@ -39,15 +39,15 @@ constexpr size_t c_pause_cycles_cascadelake{40};
 
 // REVIEW: We assume for now that our CPU runs at 3GHz. Later we could get the
 // clock speed at compile- or runtime.
-constexpr size_t c_cycles_per_ns{3};
-constexpr size_t c_ns_per_us{1000};
+static constexpr size_t c_cycles_per_ns{3};
+static constexpr size_t c_ns_per_us{1000};
 
 // REVIEW: Assume for now we are always running on x86_64 (Cascade Lake). Again,
 // we could make this configurable at compile- or runtime.
-constexpr size_t c_pause_cycles{c_pause_cycles_cascadelake};
-constexpr size_t c_pause_iterations_per_us{(c_cycles_per_ns * c_ns_per_us) / c_pause_cycles};
+static constexpr size_t c_pause_cycles{c_pause_cycles_cascadelake};
+inline constexpr size_t c_pause_iterations_per_us{(c_cycles_per_ns * c_ns_per_us) / c_pause_cycles};
 
-void spin_wait(size_t iterations)
+inline void spin_wait(size_t iterations)
 {
     for (size_t i = 0; i < iterations; ++i)
     {
